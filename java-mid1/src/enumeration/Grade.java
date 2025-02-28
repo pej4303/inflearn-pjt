723138ae56f15d3a15355f71e74a7598f080bdf6
pej4303 <pej4303@gmail.com>
2025-02-28 13:28:14 +0900

    서브모듈 테스트

--- java-mid1/src/enumeration/Grade.java
+++
@@ -1,5 +0,0 @@
-package enumeration;
-
-public enum Grade {
-    BASIC, GOLD, DIAMOND;
-}
--- java-mid1/src/lang/object/poly/Animal.java
+++
@@ -1,7 +0,0 @@
-package lang.object.poly;
-
-public class Animal {
-    public void print() {
-        System.out.println("Animal");
-    }
-}
