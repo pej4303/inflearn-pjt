b55222c4972261444ff97240c6a7ed0261aa648f
pej4303 <pej4303@gmail.com>
2025-03-06 22:13:41 +0900

    :pencil2: Repository 개발

--- portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/presentation/dto/AchievementDTO.kt
+++ portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/presentation/dto/AchievementDTO.kt
@@ -1,5 +1,11 @@
 package com.pej.portfolio_pej.presentation.dto
 
+/**
+ * data class :
+ *  - 데이터를 저장하는 용도로 설계된 클래스이다.
+ *  - Spring Boot, Kotlin 기반 프로젝트에서 DTO로 자주 활용된다.
+ *  - 속성으로 val가 권장되지만 var로 사용할 수도 있다.
+ */
 data class AchievementDTO (
     val title: String,
     val description: String,
--- portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/presentation/repository/PresentationRepository.kt
+++ portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/presentation/repository/PresentationRepository.kt
@@ -1,7 +1,37 @@
 package com.pej.portfolio_pej.presentation.repository
 
+import com.pej.portfolio_pej.domain.entity.*
+import com.pej.portfolio_pej.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository {
+class PresentationRepository(
+      private val achievementRepository: AchievementRepository
+    , private val introductionRepository: IntroductionRepository
+    , private val linkRepository: LinkRepository
+    , private val skillRepository: SkillRepository
+    , private val projectRepository: ProjectRepository
+    , private val experienceRepository: ExperienceRepository
+) {
+    /**
+     * 프레젠테이션 레이어에 필요한 기능들을 PresentationRepository에서 관리하기 위해서 만들어졌음
+     */
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+    fun getActiveIntroduction(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+    fun getActiveLink(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+    fun getActiveSkill(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+    fun getActiveProject(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+    fun getActiveExperience(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
 }
