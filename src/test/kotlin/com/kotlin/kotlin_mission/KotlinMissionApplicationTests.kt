9cca031c1b39788e18008301936e69e31713d89b
pej4303 <pej4303@gmail.com>
2025-03-08 16:24:34 +0900

    Squashed 'kotlin-mission/' content from commit d06196e
    git-subtree-dir: kotlin-mission
    git-subtree-split: d06196ea7ab78ef3f9dd4fbd18bfcf9ed35373c5

---
+++ src/test/kotlin/com/kotlin/kotlin_mission/KotlinMissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.kotlin_mission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
