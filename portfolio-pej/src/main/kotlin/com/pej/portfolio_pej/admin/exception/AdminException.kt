866385ae88d224e7e695e04b449af177b5acf9fa
pej4303 <pej4303@gmail.com>
2025-03-18 21:11:41 +0900

    :pencil2: Admin 개발 Exception

--- portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/admin/exception/AdminException.kt
+++ portfolio-pej/src/main/kotlin/com/pej/portfolio_pej/admin/exception/AdminException.kt
@@ -2,9 +2,23 @@ package com.pej.portfolio_pej.admin.exception
 
 import org.springframework.http.HttpStatus
 
-class AdminException(
+abstract class AdminException(
     httpStatus: HttpStatus,  // HTTP 상태 코드
     message: String,          // 예외 메시지
 ) : RuntimeException(message) {  // RuntimeException(message) 상속
     val httpStatus: HttpStatus = httpStatus  // httpStatus 필드 선언 및 초기화
+
+
 }
+
+/**
+ * 사용자 정의 Exception
+ */
+class AdminBadRequsetException(message: String): AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message
+)
+class AdminInternalServiceErrorException(message: String): AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message
+)
