11951d16ce3edfd7250764339768bc42792d7210
pej4303 <pej4303@gmail.com>
2025-03-26 22:22:47 +0900

    도커로 프로젝트 배포하기 진행중

--- portfolio-pej/src/test/kotlin/com/pej/portfolio_pej/PortfolioPejApplicationTests.kt
+++ portfolio-pej/src/test/kotlin/com/pej/portfolio_pej/PortfolioPejApplicationTests.kt
@@ -12,24 +12,5 @@ class PortfolioPejApplicationTests {
 	fun contextLoads() {
 	}
 
-	@Test
-	fun jasypt() {
-		val plainText = "pej!@#59685"
 
-		val encryptor = PooledPBEStringEncryptor()
-		val config = SimpleStringPBEConfig()
-		config.password = "q1w2e3"
-		config.algorithm = "PBEWithMD5AndDES"
-		config.setKeyObtentionIterations("1000")
-		config.setPoolSize("1")
-		config.providerName = "SunJCE"
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
-		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator")
-		config.stringOutputType = "base64"
-		encryptor.setConfig(config)
-		val encryptedText: String = encryptor.encrypt(plainText)
-		val decryptedText: String = encryptor.decrypt(encryptedText)
-		println(encryptedText)
-		println(decryptedText)
-	}
 }
