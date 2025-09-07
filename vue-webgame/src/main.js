6d7a2a594f32565cafe8e488f5ffc2fd5d7dd4a4
pej4303 <pej4303@gmail.com>
2025-09-07 16:19:13 +0900

    ✏️ vue 공부 6일차 내용 추가가

--- vue-webgame/src/main.js
+++ vue-webgame/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import NumberBaseball from './section03/NumberBaseball.vue'
 import ResponseCheck from './section04/ResponseCheck.vue'
 import RockScissorsPaper from './section05/RockScissorsPaper.vue';
+import LottoGenerator from './section06/LottoGenerator.vue';
 
 createApp(NumberBaseball).mount('#section03');
 createApp(ResponseCheck).mount('#section04');
 createApp(RockScissorsPaper).mount('#section05');
+createApp(LottoGenerator).mount('#section06');
